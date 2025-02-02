0a055f92d6dabe9f277703956c7d69ba51f85a6f
Tiago <tfa.antunes@gmail.com>
2025-02-02 21:53:41 +0000

    change: different solution to ex00 to be in a more c++ way

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,20 +6,20 @@
 /*   By: tialbert <tialbert@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 12:09:48 by tialbert          #+#    #+#             */
-/*   Updated: 2025/02/01 13:56:44 by tialbert         ###   ########.fr       */
+/*   Updated: 2025/02/02 21:51:20 by tialbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
 
-inline static void	putstr_upper(char *argv)
+inline static void	putstr_upper(std::string argv)
 {
-	while (*argv != 0)
-	{
-		std::cout << (char) toupper(*argv);
-		argv++;
-	}
+	int	i;
+
+	i = 0;
+	while (i < argv.length())
+		std::cout << (char) toupper(argv[i++]);
 }
 
 int	main(int argc, char **argv)
